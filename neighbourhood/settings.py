979ef9d2df5ac7ff88dfe0e956ecef87e59423db
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-03 13:26:53 +0300

    Polish

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -108,7 +108,6 @@ REST_FRAMEWORK = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
